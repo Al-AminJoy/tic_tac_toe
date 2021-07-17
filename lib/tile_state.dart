ced356f753f7b2929861157c23edfb6b7a7a969f
Al-AminJoy <alaminislam3555@gmail.com>
2021-07-17 22:55:56 +0600

    First Commit

---
+++ lib/tile_state.dart
@@ -0,0 +1,11 @@
+import 'dart:math';
+
+enum TileState{
+  EMPTY,CROSS,CIRCLE
+}
+List<List<TileState>> chunk(List<TileState> list,int size){
+  return List.generate(
+      3,
+          (index) => 
+        list.sublist(index * size,min(index * size+size, list.length)));
+}
